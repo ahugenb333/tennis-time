9255da9d1d82657fe59f914690d3bcfbea2b040c
ahugenb333 <ahugenberg9@gmail.com>
2024-02-15 10:49:47 -0600

    delete revoked api key

--- app/src/main/java/com/ahugenb/tt/api/TennisApiService.kt
+++ app/src/main/java/com/ahugenb/tt/api/TennisApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Headers
 
 interface TennisApiService {
     @Headers(
-        "X-RapidAPI-Key:3e50af0721msh73e1f3688b33897p11cfb8jsn9c9d9f0f0b2d",
+        "X-RapidAPI-Key:YOUR_KEY_HERE",
         "X-RapidAPI-Host: ultimate-tennis1.p.rapidapi.com"
     )
     @GET("live_scores")
